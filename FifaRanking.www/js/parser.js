65df316a83a4c1a3b7ff46446874c89315b95cfb
Piotr Latuszkiewicz <piotr.latuszkiewcz@gmail.com>
2016-12-15 20:50:54 +0100

    refactor solution structure

---
+++ FifaRanking.www/js/parser.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    // *** *** *** Uncomment to recalculate all (rankings, teams) *** *** ***
+
+    //$.ajax({
+    //    url: "/parser?action=recalculateAll",
+    //    type: "POST",
+    //    data: {},
+    //    success: function (data) {
+
+    //    },
+    //    error: function (jqXHR, textStatus, errorThrown) {
+    //        alert("Error! Status: " + textStatus + ", " + errorThrown);
+    //    }
+    //});
+});
---
+++ FifaRanking.www/js/view.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "/parser?action=getLatestRanking",
+        type: "POST",
+        data: {},
+        success: function (data) {
+            // console.log(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Error! Status: " + textStatus + ", " + errorThrown);
+        }
+    });
+
+    $.ajax({
+        url: "/parser?action=getTeamList",
+        type: "POST",
+        data: {},
+        success: function (data) {
+            // console.log(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Error! Status: " + textStatus + ", " + errorThrown);
+        }
+    });
+});
